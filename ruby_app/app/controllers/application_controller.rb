bd790a9d3414f6f2db8ae93b368fa3fde019f8bb
Macaya25 <afmacaya@miuandes.cl>
2022-10-07 03:39:21 -0300

    Sign out path changed and fixed.

--- ruby_app/app/controllers/application_controller.rb
+++ ruby_app/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :type, :phone_number])
       devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :password, :password_confirmation, :current_password) }
     end
 end
