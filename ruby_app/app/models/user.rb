44cf66b7af81d3f34022a102f7646829cbaffd6e
webtech <webtech@webtech-debian.webtech>
2022-10-07 16:42:40 -0300

    validaciones de correo y rut, que sean unicos y el telefono, nombre y apellido sea necesario incluir

--- ruby_app/app/models/user.rb
+++ ruby_app/app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :RUT, presence: true
+  validates :RUT, uniqueness: true
 end
