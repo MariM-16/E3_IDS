c482a97ecacc4ab5945b30dbb4eb4df353e45043
Matiasba8 <mibustos2@miuandes.cl>
2022-10-10 20:06:49 -0300

    feat(orders table)

--- ruby_app/db/seeds.rb
+++ ruby_app/db/seeds.rb
@@ -9,6 +9,6 @@ u1 = User.create(email:"comprador@gmail.com", first_name:"Comprador", last_name:
 u2 = User.create(email:"proveedor@gmail.com", first_name:"Proveedor", last_name: "P", phone_number:"99999",RUT:"2313-proveedor", user_type:'Proveedor', password:'12345678')
 u3 = User.create(email:"despachador@gmail.com", first_name:"Despachador", last_name: "D", phone_number:"99999",RUT:"11-despachador", user_type:'Despachador', password:'12345678')
 
-o1 = Order.create(status_order: "En transito", order_delivery_date: '1998-10-04 10:10:10', order_date: '1998-09-04 10:10:10', buyer_id: u1, provider_id: u2, dispatcher_id: u3)
-o2 = Order.create(status_order: "Completada", order_delivery_date: '1998-10-04 10:10:10', order_date: '1998-09-04 10:10:10', buyer_id: u1, provider_id: u2, dispatcher_id: u3)
-o3 = Order.create(status_order: "Recepción en sistema de logistica", order_delivery_date: '1998-10-04 10:10:10', order_date: '1998-09-04 10:10:10', buyer_id: u1, provider_id: u2, dispatcher_id: u3)
+o1 = Order.create(status_order: "En transito", order_delivery_date: '1998-10-04 10:10:10', order_date: '1998-09-04 10:10:10', buyer_id: u1.id, provider_id: u2.id, dispatcher_id: u3.id)
+o2 = Order.create(status_order: "Completada", order_delivery_date: '1998-10-04 10:10:10', order_date: '1998-09-04 10:10:10', buyer_id: u1.id, provider_id: u2.id, dispatcher_id: u3.id)
+o3 = Order.create(status_order: "Recepción en sistema de logistica", order_delivery_date: '1998-10-04 10:10:10', order_date: '1998-09-04 10:10:10', buyer_id: u1.id, provider_id: u2.id, dispatcher_id: u3.id)
