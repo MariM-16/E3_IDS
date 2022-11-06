2c9d8484494c29d322d1a3778f31fb094adb9b19
jlcamusa <jlcamus@miuandes.cl>
2022-11-06 01:29:34 -0300

    fixed profile and added product index

--- ruby_app/config/routes.rb
+++ ruby_app/config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
   resources :order_products
   resources :comments
   resources :products
   resources :vehicles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   devise_for :users
-  #resources :users, only: [:show]
 
   devise_scope :user do  
-      get '/users/sign_out' => 'devise/sessions#destroy'     
+    get '/users/sign_out' => 'devise/sessions#destroy'     
   end
+
+    resources :users
   
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   root "home#index"
 
